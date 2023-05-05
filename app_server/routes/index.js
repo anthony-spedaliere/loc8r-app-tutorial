61bd9378c0af3f6936ccef232258caba0c02058c
Anthony Spedaliere <avspedaliere@gmail.com>
2023-05-04 17:21:32 -0700

    set up views

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-// controller import
-const ctrlMain = require('../controllers/main');
+// controller imports
+const ctrlLocations = require('../controllers/locations');
+const ctrlOthers = require('../controllers/others');
 
-/* GET home page route. */
-router.get('/', ctrlMain.index);
+/* Locations pages */
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+
+/* Other pages */
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
